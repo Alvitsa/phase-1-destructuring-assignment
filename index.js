17df19fbc8bf093ad02b9fb86d61eb1dffaaac21
+ <+>
2024-04-02 12:33:57 +0300

    creates lab

--- index.js
+++ index.js
@@ -25,24 +25,47 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
+const [moo, neigh, baa, oink, cluck] = ['cow', 'horse', 'sheep', 'pig', 'chicken'];
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const { bessie, dolly, babe, little } = {
+  bessie: 'cow',
+  dolly: 'sheep',
+  babe: 'pig',
+  little: 'chicken',
+};
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const { blackAndWhite, black, pink } = {
+  blackAndWhite: 'cow',
+  black: 'sheep',
+  pink: 'pig',
+};
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const [red, orange, yellow, green, blue, indigo, violet] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// Arrays
+const [r, o, y, g, b, v] = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
 
-// 4. Use destructuring to assign appropriate variables using the color names.
+const [indg] = ['indigo'];
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const muppetData = {
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit",
+  nested: {
+    song2: "Moving Right Along",
+    song4: "I Hope That Something Better Comes Along",
+    job: "Host of The Muppet Show",
+    partner: "Miss Piggy",
+  }
+};
 
-// Objects
-
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const { 
+  muppetName, 
+  color, 
+  song, 
+  job, 
+  partner, 
+  nested: { song2, song4, job: nestedJob, partner: nestedPartner }
+} = muppetData;
